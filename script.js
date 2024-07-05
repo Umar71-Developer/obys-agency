2442533eed68d5b5b80fdbf33ff3769833ee3da7
Umar Hayyat <umarhayyat71.com>
2024-07-05 13:37:10 +0500

    page1 done

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
-
-var tl = gsap.timeline()
+function loadingAnimation(){
+    var tl = gsap.timeline()
 
 tl.from('.line h1' , {
     y: "150",
@@ -41,3 +40,24 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#hero1 h1,#hero2 h1, #hero3 h2, #hero4 h1",{
+    y:120,
+    stagger:0.1
+})
+tl.from("#nav",{
+    opacity:0
+})
+}
+loadingAnimation();
+
+function cursorAnimation(){
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#crsr", {
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4");
+}
+cursorAnimation();
